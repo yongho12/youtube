1eded18f61d5564aba8d619dd4a2ef31995ab392
Yongho Kim <winthisgame12@gmail.com>
2021-03-26 11:41:19 -0600

    Search CSS done

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
+import SearchHeader from './components/search_header/search_header'
 
 function App() {
   const [videos, setVidoes] = useState([]);
+  const [search, setSearch] = useState([]);
 
   useEffect(()=> {
     const requestOptions = {
@@ -17,7 +19,14 @@ function App() {
     .catch(error => console.log('error', error));
     }, []);
 
-  return <VideoList videos={videos} />;
+
+  
+  return (
+    <div className={styles.app}>
+      <SearchHeader search={search} />
+      <VideoList videos={videos} />;
+    </div>  
+  )
 }
 
 export default App;
